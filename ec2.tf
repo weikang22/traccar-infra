532d4fcce8af10930186b35724b95617540d276d
weikang <weikangsu@live.co.uk>
2026-07-05 20:17:04 +0100

    ec2.tf created and verified operational

---
+++ ec2.tf
@@ -0,0 +1,34 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "traccar" {
+  key_name   = "traccar"
+  public_key = file("~/.ssh/traccar.pub")
+}
+
+resource "aws_instance" "traccar" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.traccar.id]
+  associate_public_ip_address = true
+
+  key_name = aws_key_pair.traccar.key_name
+
+  tags = {
+    Name = "traccar-server"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "public_subnet_id" {
 output "igw_id" {
   value = aws_internet_gateway.igw.id
 }
+
+output "aws_ami" {
+  value = data.aws_ami.ubuntu.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.traccar.public_ip
+}
