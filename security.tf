ac3daf7f6bb4233276eb9bdae5503e7595b16679
weikang <weikangsu@live.co.uk>
2026-08-16 10:50:28 +0100

    infra diagram

--- security.tf
+++ security.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "traccar" {
     to_port     = 8082
     protocol    = "tcp"
 
-    cidr_blocks = [var.ssh_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress { # required to specifically re-create as Terrafrom removes the default allow all egress
